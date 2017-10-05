4fb64b005d9b211fc1b051de58a413d759dda0cf
Forcellato <31929450+Forcellato@users.noreply.github.com>
2017-10-05 17:48:52 +0200

    Add files via upload
    Client per potersi interfacciare con il date server

---
+++ DateClient.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+/*
+ * start server using:
+ * php -S localhost:8080
+ */
+
+public class DateClient {
+
+    public static void main(String[] args) throws IOException {
+        String serverAddress = "localhost"; // server string
+        
+        Socket s = new Socket(serverAddress, 9090); //Modificata la porta del servizio
+        BufferedReader input =
+            new BufferedReader(new InputStreamReader(s.getInputStream()));
+        String str = "DATA RECEIVED:";
+        do {
+            System.out.println(str);
+            str = input.readLine();
+        } while(str != null);
+    }
+}
