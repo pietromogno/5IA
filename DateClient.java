4c606fb99a8ad4594904b70cae43954dee3a569f
Enrico Da Ronche <enricodr99@gmail.com>
2017-10-05 16:20:20 +0200

    Commit per aggiornamento DateClient

--- DateClient.java
+++ DateClient.java
@@ -1,10 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package dateclient;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -15,7 +8,7 @@ public class DateClient {
 
     public static void main(String[] args) throws IOException {
         String serverAddress = "localhost"; // server string
-        
+
         Socket s = new Socket(serverAddress, 9090);
         BufferedReader input =
             new BufferedReader(new InputStreamReader(s.getInputStream()));
