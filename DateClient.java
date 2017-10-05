8bd404422550042b615d91dd99cd90662c834c0a
unknown <manuel.vivian11@gmail.com>
2017-10-05 16:33:44 +0200

    modifica file DateClient

---
+++ DateClient.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+/*
+ * start server using:
+ * php -S localhost:9090
+ */
+
+public class DateClient {
+
+    public static void main(String[] args) throws IOException {
+        String serverAddress = "localhost"; // server string
+        
+        Socket s = new Socket(serverAddress, 9090);//Modificata porta da 8080 a 9090
+        DataOutputStream dOut = new DataOutputStream(s.getOutputStream());
+        dOut.writeBytes("GET /index.php HTTP/1.0\n\n"); // minimal GET request
+        BufferedReader input =
+            new BufferedReader(new InputStreamReader(s.getInputStream()));
+        String str = "DATA RECEIVED:";
+        do {
+            System.out.println(str);
+            str = input.readLine();
+        } while(str != null);
+        dOut.close();
+    }
+}
