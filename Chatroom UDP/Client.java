0d8fb95cf4fd199366fe681bc5f470ef93e264dc
Pietro Mogno <31979560+pietromogno@users.noreply.github.com>
2017-11-30 21:09:09 +0100

    consegna udp

---
+++ Chatroom UDP/Client.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chats;
+
+/**
+ *
+ * @author Pietro Mogno 5IA
+ */
+public class Client extends javax.swing.JFrame {
+
+
+
+    public static void main(String args[]) {
+
+}
---
+++ Chatroom UDP/Server.java
@@ -0,0 +1,17 @@
+package chats;
+
+
+/**
+ *
+ * @author Pietro Mogno 5IA
+ */
+public class Server extends javax.swing.JFrame {
+
+    /**
+     * Creates new form chat_server
+     */
+
+    public static void main(String args[]) {
+
+    }
+}
