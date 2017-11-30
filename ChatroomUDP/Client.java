1c45a40920b88f9703b618d137952bc4d8e42e49
simonetrevisan <31980676+simonetrevisan@users.noreply.github.com>
2017-11-30 22:27:46 +0100

    Consegna ChatRoomUDP

---
+++ ChatroomUDP/Client.java
@@ -0,0 +1,13 @@
+
+package chatroomudp;
+
+/**
+ * @author Simone Trevisan 5^IA
+ */
+
+public class Client extends javax.swing.JFrame {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ ChatroomUDP/Server.java
@@ -0,0 +1,14 @@
+
+package chatroomudp;
+
+/**
+ *
+ * @author Simone Trevisan 5^IA
+ */
+
+public class Server extends javax.swing.JFrame {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
