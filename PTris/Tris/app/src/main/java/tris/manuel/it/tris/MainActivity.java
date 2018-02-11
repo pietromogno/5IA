30ece9445598a91dc7cdf25f08a132f574b1e06b
unknown <manuel.vivian11@gmail.com>
2018-02-11 08:59:28 +0100

    terzo commit p. tris

---
+++ PTris/Tris/app/src/main/java/tris/manuel/it/tris/MainActivity.java
@@ -0,0 +1,22 @@
+package tris.manuel.it.tris;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public void personaButton(View w){
+        startActivity(new Intent(this, Locale.class));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+}
